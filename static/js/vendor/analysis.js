36970f31142dc7dff6f5067d358d8a00893dbeeb
qiongniu23 <85395598@qq.com>
2016-09-18 12:03:06 +0800

    谭坤鹏第一版

--- static/js/vendor/analysis.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?1cb37c6b2ecd67a30f6cb15a49884068";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
